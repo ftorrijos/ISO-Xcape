0f994f9afcc5f2059d707d69c7dda0dc8f5f56ad
ftorrijos <38426547+ftorrijos@users.noreply.github.com>
2018-11-03 09:50:11 +0100

    Refractor Alejandro

---
+++ XcapeApp/src/implementacion/Login.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package implementacion;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Login {
+           //FER ESTA CLASE TE LA HE PUESTO AQUI PERO TU VERAS SI ES AQUI O NO 
+}
