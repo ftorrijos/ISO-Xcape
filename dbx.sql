7cba0901f68d58c9f92c834e83cc810d2bb9cbd8
ftorrijos <38426547+ftorrijos@users.noreply.github.com>
2018-11-04 11:24:13 +0100

    BBDD actualizada
    He creado las siguientes tables:
    grupo (actualizada)
    info_viaje (creada)
    pagos (creada)
    usuario_grupo(creada)
    responsable(actualizada)
    insertado tambien minimo de un registro por tabla.
    En grupo aparecen campos como null porque he realizado alter table y no drop&create

--- dbx.sql
+++ dbx.sql
@@ -25,7 +25,13 @@ DROP TABLE IF EXISTS `grupo`;
 CREATE TABLE `grupo` (
   `grupo_id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`grupo_id`)
+  `responsable_id` int(11) DEFAULT NULL,
+  `viaje_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`grupo_id`),
+  KEY `responsable_id` (`responsable_id`),
+  KEY `viaje_id` (`viaje_id`),
+  CONSTRAINT `grupo_ibfk_1` FOREIGN KEY (`responsable_id`) REFERENCES `responsables` (`responsable_id`),
+  CONSTRAINT `grupo_ibfk_2` FOREIGN KEY (`viaje_id`) REFERENCES `info_viaje` (`viaje_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -35,7 +41,7 @@ CREATE TABLE `grupo` (
 
 LOCK TABLES `grupo` WRITE;
 /*!40000 ALTER TABLE `grupo` DISABLE KEYS */;
-INSERT INTO `grupo` VALUES (1,'AND-01'),(2,'AND-02'),(3,'CAN-01');
+INSERT INTO `grupo` VALUES (1,'AND-01',NULL,NULL),(2,'AND-02',NULL,NULL),(3,'CAN-01',NULL,NULL);
 /*!40000 ALTER TABLE `grupo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -70,6 +76,64 @@ INSERT INTO `incidencias` VALUES (1,3,2,'El viaje es la ****, este anno repito!'
 UNLOCK TABLES;
 
 --
+-- Table structure for table `info_viaje`
+--
+
+DROP TABLE IF EXISTS `info_viaje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `info_viaje` (
+  `viaje_id` int(11) NOT NULL AUTO_INCREMENT,
+  `hotel` varchar(50) DEFAULT NULL,
+  `direccion_hotel` varchar(100) DEFAULT NULL,
+  `regimen` varchar(50) DEFAULT NULL,
+  `estacion_forfait` varchar(50) DEFAULT NULL,
+  `duracion` int(11) DEFAULT NULL,
+  PRIMARY KEY (`viaje_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `info_viaje`
+--
+
+LOCK TABLES `info_viaje` WRITE;
+/*!40000 ALTER TABLE `info_viaje` DISABLE KEYS */;
+INSERT INTO `info_viaje` VALUES (1,'SPA RESORT ANDORRA','123,GrandValira,Andorra','desayuno','grandvalira',5);
+/*!40000 ALTER TABLE `info_viaje` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pagos`
+--
+
+DROP TABLE IF EXISTS `pagos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `pagos` (
+  `pago_id` int(11) NOT NULL AUTO_INCREMENT,
+  `metodo_pago` varchar(15) DEFAULT NULL,
+  `primer_pago` varchar(15) DEFAULT NULL,
+  `segundo_pago` varchar(15) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `DNI` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`pago_id`),
+  KEY `usuario_id` (`usuario_id`),
+  CONSTRAINT `pagos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pagos`
+--
+
+LOCK TABLES `pagos` WRITE;
+/*!40000 ALTER TABLE `pagos` DISABLE KEYS */;
+INSERT INTO `pagos` VALUES (1,'Bitcoin','ok','ok',3,'47228811C');
+/*!40000 ALTER TABLE `pagos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `responsables`
 --
 
@@ -79,10 +143,9 @@ DROP TABLE IF EXISTS `responsables`;
 CREATE TABLE `responsables` (
   `responsable_id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) DEFAULT NULL,
-  `grupo_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`responsable_id`),
-  KEY `grupo_id` (`grupo_id`),
-  CONSTRAINT `responsables_ibfk_1` FOREIGN KEY (`grupo_id`) REFERENCES `grupo` (`grupo_id`)
+  `apellido` varchar(30) DEFAULT NULL,
+  `movil` int(11) DEFAULT NULL,
+  PRIMARY KEY (`responsable_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -92,7 +155,7 @@ CREATE TABLE `responsables` (
 
 LOCK TABLES `responsables` WRITE;
 /*!40000 ALTER TABLE `responsables` DISABLE KEYS */;
-INSERT INTO `responsables` VALUES (1,'Sancer',2),(2,'Cordoba',3),(3,'Goiriena',1);
+INSERT INTO `responsables` VALUES (1,'Javier','Sancerni',111222333),(2,'Alvaro','Cordoba',111222313),(3,'Ignacio','Goiriena',12222313);
 /*!40000 ALTER TABLE `responsables` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,6 +183,34 @@ LOCK TABLES `user_login` WRITE;
 /*!40000 ALTER TABLE `user_login` DISABLE KEYS */;
 INSERT INTO `user_login` VALUES (3,'FernandoTorrijos','5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8'),(1,'AlejandroGutierrez','5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8');
 /*!40000 ALTER TABLE `user_login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario_grupo`
+--
+
+DROP TABLE IF EXISTS `usuario_grupo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `usuario_grupo` (
+  `user_id` int(11) DEFAULT NULL,
+  `grupo_id` int(11) DEFAULT NULL,
+  `fecha` varchar(15) DEFAULT NULL,
+  KEY `grupo_id` (`grupo_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `usuario_grupo_ibfk_1` FOREIGN KEY (`grupo_id`) REFERENCES `grupo` (`grupo_id`),
+  CONSTRAINT `usuario_grupo_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `usuarios` (`usuario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario_grupo`
+--
+
+LOCK TABLES `usuario_grupo` WRITE;
+/*!40000 ALTER TABLE `usuario_grupo` DISABLE KEYS */;
+INSERT INTO `usuario_grupo` VALUES (1,1,NULL),(2,1,NULL),(3,1,NULL),(4,1,NULL),(5,1,NULL),(7,1,NULL);
+/*!40000 ALTER TABLE `usuario_grupo` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -155,4 +246,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-03 22:18:09
+-- Dump completed on 2018-11-04 11:21:21
