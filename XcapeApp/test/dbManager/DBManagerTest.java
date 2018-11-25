b8f949a8c1c2edf34c42dbf9d900a177779e0225
ftorrijos <38426547+ftorrijos@users.noreply.github.com>
2018-11-25 23:56:47 +0100

    doc changes

---
+++ XcapeApp/test/dbManager/DBManagerTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbManager;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author usuario
+ */
+public class DBManagerTest {
+
+    //primer test de prueba
+    @Test
+    public void testAdd1Plus1() {
+        int x = 1;
+        int y = 1;
+        assertEquals(2, (x + y));
+    }
+    
+//-----------------------------------------TEST CORRESPONDIENTES A DB_Manager------------------------------------------------
+//http://tutorials.jenkov.com/java-unit-testing/database-testing-crud.html    
+}
