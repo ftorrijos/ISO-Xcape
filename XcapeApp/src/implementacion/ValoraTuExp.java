dd6673839f4bf62ce72b6ded61c983ac39bc203c
ebenz <ebenz@DESKTOP-2IPDP2T>
2018-11-26 23:27:16 +0100

    añadido valoracion

--- XcapeApp/src/implementacion/ValoraTuExp.java
+++ XcapeApp/src/implementacion/ValoraTuExp.java
@@ -5,10 +5,68 @@
  */
 package implementacion;
 
+import dBManager.DBManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import modelo.Responsable;
+import modelo.Valoracion;
+
 /**
  *
  * @author Alejandro
  */
 public class ValoraTuExp {
     
+     public static Connection c = DBManager.getConnection();
+    
+    
+     public String listarValoracionNotaViaje(int viaje_id) throws SQLException{
+
+            String sql = "SELECT nota FROM valora_experiencia WHERE viaje_id=?;";
+            PreparedStatement prep = c.prepareStatement(sql);
+            prep.setInt(1, viaje_id);
+            ResultSet rs = prep.executeQuery();
+            while(rs.next()){
+            String nota = rs.getString("nota");
+            return nota;
+            }
+            
+            rs.close();
+            prep.close();
+
+            return null;
+        
+        }
+   
+    
+    
+    public static void listarValoracion() throws SQLException {
+
+        try {
+            Statement stmt5 = c.createStatement();
+            String sql5 = "SELECT * FROM valoracion;";		
+            ResultSet rs = stmt5.executeQuery(sql5);
+            System.out.println("Lista de Valoracion: ");
+            
+            while (rs.next()) {
+                int valora_id = rs.getInt("valoracion_id");
+                int usuario_id = rs.getInt("usuario_id");
+                int viaje_id = rs.getInt("viaje_id");
+                int nota = rs.getInt("nota");
+                String si_no = rs.getString("si_no");
+               
+                Valoracion valoracion = new Valoracion(valora_id, usuario_id, viaje_id,nota, si_no);
+                System.out.println(valoracion);
+            }
+            rs.close();
+            stmt5.close();
+ 
+        } catch (SQLException e) {
+        }
+
+    }
 }
+
---
+++ XcapeApp/src/modelo/Valoracion.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import dBManager.DBManager;
+
+/**
+ *
+ * @author ebenz
+ */
+public class Valoracion {
+ private int valora_id,usuario_id, viaje_id,nota;
+    private String si_no;
+    DBManager db = new DBManager();
+
+    public Valoracion(int valora_id, int usuario_id, int viaje_id, int nota, String si_no) {
+        this.valora_id = valora_id;
+        this.usuario_id=usuario_id;
+        this.viaje_id=viaje_id;
+        this.nota=nota;
+        this.si_no=si_no;
+    }
+     public Valoracion(int usuario_id, int viaje_id, int nota, String si_no) {
+       this.usuario_id=usuario_id;
+        this.viaje_id=viaje_id;
+        this.nota=nota;
+        this.si_no=si_no;              
+    }
+
+       /**
+     * @return the valora_id
+     */
+    public int getValora_id() {
+        return valora_id;
+    }
+
+    /**
+     * @param valora_id the valora_id to set
+     */
+    public void setValora_id(int valora_id) {
+        this.valora_id = valora_id;
+    }
+
+  public int getUsuario_id(){
+      return usuario_id;
+  }
+  
+  public void setUsuario_id(int usuario_id){
+      this.usuario_id = usuario_id;
+  }
+
+  public int getViaje_id(){
+      return viaje_id;
+  }
+  
+  public void setViaje_id(int viaje_id){
+      this.usuario_id = viaje_id;
+  }
+  
+    public int getNota(){
+        return nota;
+    }
+      /**
+     * @param nota the nota to set
+     */
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+    public String getSi_no(){
+      return si_no;
+  }
+  
+  public void setSi_no(String si_no){
+      this.si_no = si_no;
+  }
+    
+
+   
+
+     @Override
+    public String toString() {
+     return "valoracion {"+"valoracion id="+getValora_id()+", nota="+getNota()+" viaje id "+getViaje_id()+ " , usuario_id=" + getUsuario_id() +"gusta"+ getSi_no()+ '}';
+        
+    }
+    
+}
+
