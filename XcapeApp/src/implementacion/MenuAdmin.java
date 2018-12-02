750231dd2b17e822279445c6d5b6ade786894fca
Alex <alejandrogutierrezyermo@yahoo.es>
2018-12-02 17:18:39 +0100

    insertar usuario como admin

--- XcapeApp/src/implementacion/MenuAdmin.java
+++ XcapeApp/src/implementacion/MenuAdmin.java
@@ -10,6 +10,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.Scanner;
 import modelo.*;
 
@@ -74,6 +75,7 @@ public class MenuAdmin {
                 Menuadmin(usuario_id);
                 break;
             case 10:
+                insertar_usuario();
                 Menuadmin(usuario_id);
                 break;
             case 11:
@@ -251,5 +253,28 @@ public class MenuAdmin {
         Evento evento = new Evento(nombre,direccion,ciudad,fecha,0);
         
         return evento;
+    }public void insertar_usuario() {
+        System.out.println("\tREGISTRO:");
+        System.out.println("Por favor, introduzca los siguientes datos:");
+        try {
+            BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Nombre: ");
+            String nombre = consola.readLine();
+            System.out.println("Apellido:");
+            String apellido = consola.readLine();
+            System.out.println("Fecha de nacimiento en formato AAAA/MM/DD");
+            Date fecha_nacimiento = new Date(consola.readLine());
+            System.out.println("DNI:");
+            String dni = consola.readLine();
+            System.out.println("Correo electrónico:");
+            String correo = consola.readLine();
+            System.out.println("Movil: ");
+            int movil = Integer.parseInt(consola.readLine());
+            Usuario usuario = new Usuario(movil, nombre, apellido, dni, correo, fecha_nacimiento);
+            DBManager.insertarUsuarios(usuario);
+           //System.out.println(usuario.getNombre() + " bienvenido a la eXperiencia Xcape");
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
